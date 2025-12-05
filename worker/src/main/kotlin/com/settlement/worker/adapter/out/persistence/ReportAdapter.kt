e2d843fc876c04682e6caaeae26c3334ceafa977
armada <armada@easyshop.co.kr>
2025-12-05 17:40:54 +0900

    보고서 리포트의 품질을 UP

--- worker/src/main/kotlin/com/settlement/worker/adapter/out/persistence/ReportAdapter.kt
+++ worker/src/main/kotlin/com/settlement/worker/adapter/out/persistence/ReportAdapter.kt
@@ -2,6 +2,7 @@ package com.settlement.worker.adapter.out.persistence
 
 
 
+import com.settlement.common.extensions.getLogger
 import com.settlement.worker.application.port.out.SaveReportPort
 import com.settlement.core.domain.model.SettlementReport
 import com.settlement.worker.adapter.out.persistence.mongo.repository.ReportMongoRepository
@@ -13,8 +14,11 @@ class ReportAdapter(
     private val reportRepository: ReportMongoRepository,
     private val reportMapper    : ReportMapper,
 ): SaveReportPort {
+    private val logger = getLogger()
+
     override fun saveReport(report: SettlementReport): SettlementReport {
         val document = reportMapper.toDocument(report)
+        logger.info("레포트 저장 = {}", document.toString())
         val savedDocument = reportRepository.save(document)
         return reportMapper.toDomain(savedDocument)
     }
