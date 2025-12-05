c9db46eabd0f6c6409eaaec56121c45d59a4a48f
armada <armada@easyshop.co.kr>
2025-12-05 10:21:05 +0900

    ReportEventWorker 오류 수정

--- worker/build.gradle.kts
+++ worker/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":core")) // core (DB/도메인) 가져다 쓰기
+    implementation(project(":common"))
 
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.springframework.boot:spring-boot-starter-web") // RestClient용
